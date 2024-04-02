6f800b282eaebb58f69375c9f9ca1f0ed36b223b
DianaKryzhanivska <di_parkhomenko@ukr.net>
2024-04-03 01:47:35 +0300

    AddPharmacyPromo section

---
+++ src/components/Home/AddPharmacyPromo/AddPharmacyPromo.styled.js
@@ -0,0 +1,100 @@
+import styled from "styled-components";
+import { CommonContainer } from "styles/GlobalStyles";
+
+export const Container = styled(CommonContainer)`
+  padding-bottom: 91px;
+  background: #f7f8fa;
+`;
+
+export const Wrapper = styled.div`
+  padding: 40px 20px 20px 20px;
+  border-radius: 32px;
+  background: ${({ theme }) => theme.colors.green};
+
+  @media only screen and (min-width: 768px) {
+    padding: 104px 48px 40px;
+  }
+
+  @media only screen and (min-width: 1440px) {
+    padding: 40px 40px 40px 80px;
+    display: flex;
+    gap: 19px;
+    align-items: center;
+  }
+`;
+
+export const Title = styled.h2`
+  color: #f1f1f1;
+  font-size: 28px;
+  font-weight: 600;
+  line-height: 1.14em;
+  letter-spacing: -0.28px;
+  margin-bottom: 20px;
+
+  @media only screen and (min-width: 768px) {
+    font-size: 48px;
+    letter-spacing: -0.48px;
+    margin-bottom: 24px;
+  }
+
+  @media only screen and (min-width: 1440px) {
+    width: 501px;
+  }
+`;
+
+export const Text = styled.p`
+  color: #f1f1f1;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.28em;
+  margin-bottom: 40px;
+
+  @media only screen and (min-width: 768px) {
+    width: 488px;
+    font-size: 16px;
+    line-height: 1.25em;
+  }
+`;
+
+export const Btn = styled.button`
+  background: transparent;
+  margin-bottom: 39px;
+
+  & a {
+    display: block;
+    border-radius: 60px;
+    padding: 13px 32px;
+    border: 1px solid rgba(241, 241, 241, 0.5);
+    color: #f1f1f1;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 1.28em;
+  }
+
+  @media only screen and (min-width: 768px) {
+    margin-bottom: 83px;
+
+    & a {
+      padding: 13px 50px;
+    }
+  }
+
+  @media only screen and (min-width: 1440px) {
+    margin-bottom: 0;
+  }
+`;
+
+export const ImgBox = styled.div`
+  & img {
+    border-radius: 0px 24px 24px 0px;
+    width: 310px;
+    height: 335px;
+  }
+
+  @media only screen and (min-width: 768px) {
+    & img {
+      width: 633px;
+      height: 406px;
+    }
+  }
+`;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import MainBanner from "./MainBanner/MainBanner";
 import PromoBanners from "./PromoBanners/PromoBanners";
+import AddPharmacyPromo from "./AddPharmacyPromo/AddPharmacyPromo";
 
 const Home = () => {
   return (
     <>
       <MainBanner />
       <PromoBanners />
+      <AddPharmacyPromo />
     </>
   );
 };
