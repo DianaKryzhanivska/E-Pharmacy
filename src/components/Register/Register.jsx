0f75d716d86ff215bda4b830594e1f205e270527
DianaKryzhanivska <di_parkhomenko@ukr.net>
2024-04-03 17:45:51 +0300

    register page desktop

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -5,6 +5,7 @@ import {
   ImgWrapper,
   InputBox,
   LogoWrapper,
+  MainWrapper,
   SubmitBtn,
   Title,
   TitleBox,
@@ -19,8 +20,8 @@ import { useMediaQuery } from "react-responsive";
 
 const Register = () => {
   const isMobile = useMediaQuery({ query: "(max-width: 767px)" });
-  const isTablet = useMediaQuery({
-    query: "(min-width: 768px) and (max-width: 1439px)",
+  const isTabletOrDesktop = useMediaQuery({
+    query: "(min-width: 768px)",
   });
   return (
     <>
@@ -28,38 +29,40 @@ const Register = () => {
         <LogoWrapper>
           <Logo />
         </LogoWrapper>
-        <TitleBox>
-          <Title>
-            Your medication, delivered Say goodbye to all{" "}
-            <span>your healthcare</span> worries with us
-          </Title>
-          <ImgWrapper>
-            {isMobile && (
-              <img
-                srcSet={`${usualMob} 1x, ${retinaMob} 2x`}
-                alt="illustration"
-              />
-            )}
-            {isTablet && (
-              <img
-                srcSet={`${usualTab} 1x, ${retinaTab} 2x`}
-                alt="illustration"
-              />
-            )}
-          </ImgWrapper>
-        </TitleBox>
-        <form>
-          <InputBox>
-            <input type="text" placeholder="User Name" />
-            <input type="text" placeholder="Email address" />
-            <input type="text" placeholder="Phone number" />
-            <input type="password" placeholder="Password" />
-          </InputBox>
-          <BtnBox>
-            <SubmitBtn type="submit">Register</SubmitBtn>
-            <NavLink to="/login">Already have an account?</NavLink>
-          </BtnBox>
-        </form>
+        <MainWrapper>
+          <TitleBox>
+            <Title>
+              Your medication, delivered Say goodbye to all{" "}
+              <span>your healthcare</span> worries with us
+            </Title>
+            <ImgWrapper>
+              {isMobile && (
+                <img
+                  srcSet={`${usualMob} 1x, ${retinaMob} 2x`}
+                  alt="illustration"
+                />
+              )}
+              {isTabletOrDesktop && (
+                <img
+                  srcSet={`${usualTab} 1x, ${retinaTab} 2x`}
+                  alt="illustration"
+                />
+              )}
+            </ImgWrapper>
+          </TitleBox>
+          <form>
+            <InputBox>
+              <input type="text" placeholder="User Name" />
+              <input type="text" placeholder="Email address" />
+              <input type="text" placeholder="Phone number" />
+              <input type="password" placeholder="Password" />
+            </InputBox>
+            <BtnBox>
+              <SubmitBtn type="submit">Register</SubmitBtn>
+              <NavLink to="/login">Already have an account?</NavLink>
+            </BtnBox>
+          </form>
+        </MainWrapper>
       </Container>
     </>
   );
--- src/components/Register/Register.styled.js
+++ src/components/Register/Register.styled.js
@@ -11,13 +11,29 @@ export const Container = styled(CommonContainer)`
     padding-bottom: 250px;
     padding-right: 122px;
   }
+
+  @media only screen and (min-width: 768px) {
+    padding: 28px 98px 262px 100px;
+  }
 `;
 
 export const LogoWrapper = styled.div`
+  width: 135px;
   margin-bottom: 106px;
 
   @media only screen and (min-width: 768px) {
+    width: 172px;
     margin-bottom: 168px;
+  }
+  @media only screen and (min-width: 1440px) {
+    margin-bottom: 226px;
+  }
+`;
+
+export const MainWrapper = styled.div`
+  @media only screen and (min-width: 1440px) {
+    display: flex;
+    gap: 54px;
   }
 `;
 
