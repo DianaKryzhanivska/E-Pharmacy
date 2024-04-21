2a1f2c691bfb309df09907397aa9dbdc2d55fb12
DianaKryzhanivska <di_parkhomenko@ukr.net>
2024-04-21 17:10:07 +0300

    header navlinks update

--- src/components/Header/NavLinks/NavLinks.jsx
+++ src/components/Header/NavLinks/NavLinks.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { NavList } from "./NavLinks.styled";
+import { NavBox, NavList, Wrapper } from "./NavLinks.styled";
+import sprite from "../../../images/sprite.svg";
+import { useMediaQuery } from "react-responsive";
 
 const NavLinks = () => {
+  const isMobileOrTablet = useMediaQuery({ query: "(max-width: 1439px)" });
   return (
     <>
-      <NavList>
-        <li>
-          <NavLink to="/home">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/medicine-store">Medicine store</NavLink>
-        </li>
-        <li>
-          <NavLink to="/medicine">Medicine</NavLink>
-        </li>
-      </NavList>
+      <Wrapper>
+        {isMobileOrTablet ? (
+          <svg>
+            <use href={`${sprite}#nav-vertical`} />
+          </svg>
+        ) : (
+          <svg>
+            <use href={`${sprite}#nav-horyzontal`} />
+          </svg>
+        )}
+        <NavBox>
+          <NavList>
+            <li>
+              <NavLink to="/home">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/medicine-store">Medicine store</NavLink>
+            </li>
+            <li>
+              <NavLink to="/medicine">Medicine</NavLink>
+            </li>
+          </NavList>
+        </NavBox>
+      </Wrapper>
     </>
   );
 };
--- src/components/Header/NavLinks/NavLinks.styled.js
+++ src/components/Header/NavLinks/NavLinks.styled.js
@@ -1,41 +1,62 @@
 import styled from "styled-components";
 
-export const NavList = styled.ul`
+export const Wrapper = styled.div`
+  position: relative;
   display: flex;
-  flex-direction: column;
-  gap: 34px;
+  align-items: center;
+  justify-content: center;
   margin-bottom: 302px;
-
-  & li {
-    text-align: center;
-  }
-
-  & a {
-    border-radius: 24px;
-    padding: 8px 12px;
-    font-size: 14px;
-    font-weight: 400;
-    background: ${({ theme }) => theme.colors.white};
-    color: ${({ theme }) => theme.colors.gray};
-  }
-
-  & a.active {
-    background: ${({ theme }) => theme.colors.green};
-    color: ${({ theme }) => theme.colors.white};
-    border: 1px solid ${({ theme }) => theme.colors.white};
-  }
 
   @media only screen and (min-width: 768px) {
     margin-bottom: 422px;
   }
 
   @media only screen and (min-width: 1440px) {
-    flex-direction: row;
     margin-bottom: 0;
-    gap: 48px;
+
+    & svg {
+      width: 344px;
+      height: 46px;
+    }
+  }
+`;
+
+export const NavBox = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const NavList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 34px;
+
+  & li {
+    text-align: center;
+    font-size: 14px;
+    font-weight: 400;
+    color: ${({ theme }) => theme.colors.gray};
+  }
+
+  & a {
+    padding: 8px 18px;
+    border-radius: 24px;
+  }
+
+  & a.active {
+    background: ${({ theme }) => theme.colors.green};
+    color: ${({ theme }) => theme.colors.white};
+  }
+
+  @media only screen and (min-width: 1440px) {
+    flex-direction: row;
+    gap: 28px;
 
     & a {
-      background: ${({ theme }) => theme.colors.white};
+      padding: 8px 12px;
+      min-width: 80px;
     }
 
     & a.active {
